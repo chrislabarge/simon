bef036bb7e72178ea1cb14ce35096f70a43dac67
WalterPeck <clabvessels@gmail.com>
2015-02-03 21:48:08 -0500

    .to_i for i

--- app/helpers/game_pages_helper.rb
+++ app/helpers/game_pages_helper.rb
@@ -36,7 +36,7 @@ def high_index
 	set_high_scores #this is so i can access the high_scores array
 	for i in @@high_scores do
 	 if i.to_i < score
-	  return @@high_scores.index(i.to_i)
+	  return @@high_scores.index(i)
 	 else
 	 	nil
 	 end 
