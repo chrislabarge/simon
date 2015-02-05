9fcf741c0d86026d83a29fd6e003dab1c6a91c29
WalterPeck <clabvessels@gmail.com>
2015-02-05 00:36:30 -0500

    figuring index stuff

--- app/helpers/game_pages_helper.rb
+++ app/helpers/game_pages_helper.rb
@@ -7,12 +7,12 @@ def high_index(high_scores)
 	for i in high_scores do
 	 return score
 	 
-	 #if score > i 
-	 #		return high_scores.index(i)
-	 #else
-	 #		return false
-	
-	# end		
+	 if score > i 
+	 		return high_scores.index(i)
+	 else
+	 		nil
+	 end		
+			
 	end 
 		
 end	
