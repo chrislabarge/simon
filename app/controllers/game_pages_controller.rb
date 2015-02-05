185b109827d5fd6be91faa066b06d04ff6bd474b
WalterPeck <clabvessels@gmail.com>
2015-02-04 20:49:09 -0500

    trying to get the scores to work in the controller like i did in the computer-server branch

--- app/controllers/game_pages_controller.rb
+++ app/controllers/game_pages_controller.rb
@@ -37,6 +37,20 @@ class GamePagesController < ApplicationController
 ######################
 
 	def scores
+		@high_score_array = []
+		@high_scores = []
+		@high_names = []	
+	
+	
+  	high_txt = File.open("app/models/high_scores.txt", "r")
+		lines = high_txt.readlines
+		high_txt.close
+		
+		lines.each{ |line| @high_score_array.push(line.chomp) }
+		
+		@high_names = @high_score_array[0..9]
+		@high_scores = @high_score_array[10..19]
+	
 	end	
 
 	def new_score
