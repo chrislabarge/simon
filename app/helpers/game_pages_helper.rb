ac8d947910ef34a5bc37b706f140eae4e37cf717
WalterPeck <clabvessels@gmail.com>
2015-02-05 00:13:07 -0500

    something is up with the way i made the high_index array

--- app/helpers/game_pages_helper.rb
+++ app/helpers/game_pages_helper.rb
@@ -5,12 +5,7 @@ module GamePagesHelper
 def high_index(high_scores)
 	
 	for i in high_scores do
-	 if i.to_i < score
-	  return high_scores.index(i)
-	 else
-	  nil
-	 end 
-		return false
+	 puts i
 	end 
 		
 end	
